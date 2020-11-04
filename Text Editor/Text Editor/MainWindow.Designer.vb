6cdf2c4c3731d5348063be541494fc1938a525eb
Josh Face <54547819+joshface-uk@users.noreply.github.com>
2020-11-04 20:35:55 +0000

    Add files via upload

---
+++ Text Editor/Text Editor/MainWindow.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MainMenu1 = New System.Windows.Forms.MainMenu(Me.components)
+        Me.MenuItem1 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem6 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem7 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem8 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem9 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem10 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem11 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem12 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem13 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem14 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem15 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem2 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem3 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem4 = New System.Windows.Forms.MenuItem()
+        Me.MenuItem5 = New System.Windows.Forms.MenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'MainMenu1
+        '
+        Me.MainMenu1.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.MenuItem1, Me.MenuItem2, Me.MenuItem3, Me.MenuItem4, Me.MenuItem5})
+        '
+        'MenuItem1
+        '
+        Me.MenuItem1.Index = 0
+        Me.MenuItem1.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.MenuItem6, Me.MenuItem7, Me.MenuItem8, Me.MenuItem9, Me.MenuItem10, Me.MenuItem11, Me.MenuItem12, Me.MenuItem13, Me.MenuItem14, Me.MenuItem15})
+        Me.MenuItem1.Text = "File"
+        '
+        'MenuItem6
+        '
+        Me.MenuItem6.Index = 0
+        Me.MenuItem6.Text = "New"
+        '
+        'MenuItem7
+        '
+        Me.MenuItem7.Index = 1
+        Me.MenuItem7.Text = "New Window"
+        '
+        'MenuItem8
+        '
+        Me.MenuItem8.Index = 2
+        Me.MenuItem8.Text = "Open..."
+        '
+        'MenuItem9
+        '
+        Me.MenuItem9.Index = 3
+        Me.MenuItem9.Text = "Save"
+        '
+        'MenuItem10
+        '
+        Me.MenuItem10.Index = 4
+        Me.MenuItem10.Text = "Save As..."
+        '
+        'MenuItem11
+        '
+        Me.MenuItem11.Index = 5
+        Me.MenuItem11.Text = "-"
+        '
+        'MenuItem12
+        '
+        Me.MenuItem12.Index = 6
+        Me.MenuItem12.Text = "Page Setup"
+        '
+        'MenuItem13
+        '
+        Me.MenuItem13.Index = 7
+        Me.MenuItem13.Text = "Print..."
+        '
+        'MenuItem14
+        '
+        Me.MenuItem14.Index = 8
+        Me.MenuItem14.Text = "-"
+        '
+        'MenuItem15
+        '
+        Me.MenuItem15.Index = 9
+        Me.MenuItem15.Text = "Exit"
+        '
+        'MenuItem2
+        '
+        Me.MenuItem2.Index = 1
+        Me.MenuItem2.Text = "Edit"
+        '
+        'MenuItem3
+        '
+        Me.MenuItem3.Index = 2
+        Me.MenuItem3.Text = "View"
+        '
+        'MenuItem4
+        '
+        Me.MenuItem4.Index = 3
+        Me.MenuItem4.Text = "Format"
+        '
+        'MenuItem5
+        '
+        Me.MenuItem5.Index = 4
+        Me.MenuItem5.Text = "Help"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 576)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1004, 22)
+        Me.StatusStrip1.TabIndex = 0
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(1004, 576)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.WordWrap = False
+        '
+        'MainWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1004, 598)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Menu = Me.MainMenu1
+        Me.Name = "MainWindow"
+        Me.Text = "Text Editor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MainMenu1 As MainMenu
+    Friend WithEvents MenuItem1 As MenuItem
+    Friend WithEvents MenuItem6 As MenuItem
+    Friend WithEvents MenuItem7 As MenuItem
+    Friend WithEvents MenuItem8 As MenuItem
+    Friend WithEvents MenuItem9 As MenuItem
+    Friend WithEvents MenuItem10 As MenuItem
+    Friend WithEvents MenuItem11 As MenuItem
+    Friend WithEvents MenuItem12 As MenuItem
+    Friend WithEvents MenuItem13 As MenuItem
+    Friend WithEvents MenuItem14 As MenuItem
+    Friend WithEvents MenuItem15 As MenuItem
+    Friend WithEvents MenuItem2 As MenuItem
+    Friend WithEvents MenuItem3 As MenuItem
+    Friend WithEvents MenuItem4 As MenuItem
+    Friend WithEvents MenuItem5 As MenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
